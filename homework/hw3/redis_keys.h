f3fee3ef470912095645a8d00342880cdf3ad7f5
Yuhan Liu <150732885+liuyuhan-serena@users.noreply.github.com>
2026-05-07 15:55:06 -0700

    Delete homework directory

--- homework/hw3/redis_keys.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// - read:
-const std::string JOINT_ANGLES_KEY = "sai::sensors::PANDA::joint_positions";
-const std::string JOINT_VELOCITIES_KEY = "sai::sensors::PANDA::joint_velocities";
-// - write
-const std::string JOINT_TORQUES_COMMANDED_KEY = "sai::commands::PANDA::control_torques";
-const std::string GRAVITY_COMP_ENABLED_KEY = "sai::simviz::gravity_comp_enabled";
